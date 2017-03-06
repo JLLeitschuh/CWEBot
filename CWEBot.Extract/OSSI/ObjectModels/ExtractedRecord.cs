06e9a9af8e6a981e6ebea40b89395e1616a77467
Allister Beharry <allister.beharry@gmail.com>
2017-03-05 23:16:48 -0400

    Rename Models dir.

---
+++ CWEBot.Extract/OSSI/ObjectModels/ExtractedRecord.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using CWEBot.Interfaces;
+namespace CWEBot.Extract.OSSIndex
+{
+    public class ExtractedRecord : IRecord
+    {
+        #region Properties
+        [JsonProperty("pm")]
+        public string PackageManager { get; set; }
+
+        [JsonProperty("pid")]
+        public long PackageId { get; set; }
+
+        [JsonProperty("name")]
+        public string PackageName { get; set; }
+
+        public long VulnerabilityId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string[] References { get; set; }
+
+        public DateTime Published { get; set; }
+
+        [JsonIgnore]
+        public int? CWEId { get; set; }
+
+        [JsonProperty("updated")]
+        public DateTime Updated { get; set; }
+        #endregion
+
+        #region Methods
+        public static implicit operator Record(ExtractedRecord er)
+        {
+            return new Record()
+            {
+                CWEId = er.CWEId,
+                Title = er.Title,
+                Description = er.Description,
+                Published = er.Published,
+                References = er.References,
+                VulnerabilityId = er.VulnerabilityId
+            };
+        }
+        #endregion
+    }
+    public class ExtractedRecordComparator : IEqualityComparer<ExtractedRecord>
+    {
+        public bool Equals(ExtractedRecord r1, ExtractedRecord r2)
+        {
+            return r1.VulnerabilityId == r2.VulnerabilityId;
+
+        }
+
+        public int GetHashCode(ExtractedRecord r)
+        {
+            return r.VulnerabilityId.GetHashCode();
+        }
+    }
+}
